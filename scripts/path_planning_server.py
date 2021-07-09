8d7938f7b0e6b36f960503455bb1b229a06714f9
Isaac Gaetani Alvarez <gaetanialvarez@gmail.com>
2021-07-09 15:18:54 -0300

    Criação do server para calcular o path planning do coverage

---
+++ scripts/path_planning_server.py
@@ -0,0 +1,4 @@
+from artigo_framework.path_planning_server_utils import path_planning_server
+
+if __name__ == "__main__":
+    path_planning_server()
---
+++ src/artigo_framework/path_planning_server_utils.py
@@ -0,0 +1,23 @@
+import rospy
+from artigo_framework.srv import PathPlanning, PathPlanningResponse
+from artigo_framework.path_planning import Ponto, Camera, Mapa, PathPlanner
+
+def handle_path_planning(requisicao):
+    mapa_inicio             = Ponto(requisicao.mapa_inicio_x, requisicao.mapa_inicio_y)
+    mapa_largura            = requisicao.mapa_largura
+    mapa_altura             = requisicao.mapa_altura
+    camera_largura          = requisicao.camera_largura
+    camera_altura           = requisicao.camera_altura
+
+    mapa                    = Mapa(mapa_inicio, mapa_largura, mapa_altura)
+    camera                  = Camera(camera_largura, camera_altura)
+    caminho                 = PathPlanner(mapa, camera)
+    caminho_x, caminho_y    = caminho.coordenadas()
+
+    return PathPlanningResponse(caminho_x, caminho_y)
+
+def path_planning_server():
+    rospy.init_node('path_planning_server')
+    s = rospy.Service('path_planning', PathPlanning, handle_path_planning)
+    print("Path planning server started")
+    rospy.spin()
