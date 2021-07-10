3fc81d6ff7df394e033bdb1bc554f6f9adb5302d
Isaac Gaetani Alvarez <gaetanialvarez@gmail.com>
2021-07-10 12:23:39 -0300

    Adição do algae_coord_service

---
+++ scripts/algae_coord_service.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python3
+from __future__ import print_function
+from artigo_framework.srv import camera_uav, camera_uavResponse
+import rospy
+import numpy as np
+import matplotlib.pyplot as plt
+import message_filters
+
+from sensor_msgs.msg import Range, Image, CameraInfo
+from std_msgs.msg import String, Int32
+from std_msgs.msg import Float64MultiArray
+from nav_msgs.msg import Odometry
+
+
+import cv2
+import os
+from cv_bridge import CvBridge
+
+############################ Functions ############################
+
+def stationary_camera_transform(point, alturaZ ):
+    K = [215.6810060961547, 0.0, 376.5, 0.0, 215.6810060961547, 240.5, 0.0, 0.0, 1.0]
+
+    baseTerrestreAltura = 0.000009 # 0.5
+
+    Z = alturaZ - baseTerrestreAltura # Distancia do solo
+
+    dX = (point[0] - K[2]) * Z / K[0]
+    dY = (point[1] - K[5]) * Z / K[4]
+
+    dist = (dX, dY)
+    return dist
+
+def algae_detector(img):
+    #print(img)
+    img = img[:,:,[0,1,2]]
+    img_ch0 = img[:,:,0]
+#thr
+    min_thr = 20
+    _, thr = cv2.threshold(img_ch0, min_thr, 255, cv2.THRESH_BINARY)
+#blur
+    size_b = 5
+    kernel_blur = np.ones((size_b, size_b),np.float32)/size_b*size_b
+    dst = cv2.filter2D(thr, -1, kernel_blur)
+#closing
+    size_c = 10
+    kernel_closing = np.ones((size_c, size_c),np.float32)/size_c*size_c
+    img_binary = cv2.morphologyEx(dst, cv2.MORPH_CLOSE, kernel_closing)
+
+    mostTrue = thr.shape[0]*thr.shape[1]*255*0.98 # 98% of the image's pixels have the same color (255)
+    mostFalse = thr.shape[0]*thr.shape[1]*255*0.02    #  2% of the image's pixels have the same color (255)
+    img_pixels = thr.shape[0]*thr.shape[1]
+    if (thr.sum() >= mostTrue) or (thr.sum() <= mostFalse):
+        #print("Is the world blue?")
+        B_sum = img[:,:,2].sum()
+        G_sum = img[:,:,1].sum()
+        R_sum = img[:,:,0].sum()
+
+        red_limit = 60
+        blue_limit = 150
+        if (R_sum < red_limit*thr.shape[0]*thr.shape[1]) and (B_sum > blue_limit*thr.shape[0]*thr.shape[1]) :
+            #print("yes, it is!")
+            img_binary = np.zeros(thr.shape)
+
+    img2 = cv2.cvtColor(img_binary, cv2.COLOR_GRAY2RGB)
+    #print(img2.shape)
+    contours, hierarchy = cv2.findContours(img_binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #_, contours, _ = cv2.findContours(img2, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
+    centres = []
+    for i in range(len(contours)):
+        moments = cv2.moments(contours[i])
+        centres.append((int(moments['m10']/moments['m00']), int(moments['m01']/moments['m00'])))
+    #print(centres)
+    return centres
+
+################################ Service #######################################
+def handle_goals(req):
+    print(req)
+    image_ros = req.image_sub
+    location = req.GPS_sub
+    bridge = CvBridge()
+    image = bridge.imgmsg_to_cv2(image_ros, desired_encoding='passthrough')
+    classified = algae_detector(image)
+    uav_z = location.pose.pose.position.z
+    uav_x = location.pose.pose.position.x
+    uav_y = location.pose.pose.position.y
+    #pub.publish(bridge.cv2_to_imgmsg(classified))
+    x_list = []
+    y_list = []
+    for i in classified:
+        p = stationary_camera_transform((i[0], 480-i[1]), uav_z)
+        algae_coord = (round(uav_x+p[0]+116,1), round(uav_y+p[1]+120, 1) )
+        x_list.append(algae_coord[0])
+        y_list.append(algae_coord[1])
+        print(algae_coord)
+
+    return camera_uavResponse(x_list, y_list)
+
+def add_two_ints():
+	rospy.init_node('algae_to_coord_server') #### Create server node
+
+### Create service: the first argument is the service name, the second the srv declared on line 3 for variables, the last is the callback
+	s = rospy.Service('algae_to_coord', camera_uav, handle_goals)
+	print("Ready to row.")
+	rospy.spin()
+
+if __name__ == "__main__":
+	add_two_ints()
--- src/artigo_framework/gps_client_utils.py
+++ src/artigo_framework/gps_client_utils.py
@@ -1,12 +1,16 @@
 import rospy
+import message_filters
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseWithCovarianceStamped
+from sensor_msgs.msg import Image
+from artigo_framework.srv import camera_uav
 
 class GPSClient:
     def __init__(self, opcao):
-        self.x = 0.0
-        self.y = 0.0
-        self.z = 0.0
+        self.x              = 0.0
+        self.y              = 0.0
+        self.z              = 0.0
+        self.flock_coord    = []
 
         if opcao == "global_position":
             rospy.Subscriber(
@@ -19,6 +23,11 @@ class GPSClient:
                 "/uav1/ground_truth_pose"   ,
                 PoseWithCovarianceStamped   ,
                 self.ground_truth_callback  )
+        
+        image_sub   = message_filters.Subscriber('/uav1/bluefox_optflow/image_raw', Image)
+        GPS_sub     = message_filters.Subscriber('/uav1/mavros/global_position/local', Odometry)
+        ts          = message_filters.ApproximateTimeSynchronizer([image_sub, GPS_sub], 10, 0.05)
+        ts.registerCallback(self.callbackImg_coord)
 
     def global_position_callback(self, data):
         self.x = data.pose.pose.position.x
@@ -29,6 +38,24 @@ class GPSClient:
         self.x = data.pose.pose.position.x
         self.y = data.pose.pose.position.y
         self.z = data.pose.pose.position.z
+    
+    def callbackImg_coord(self, img, coord):
+        print('call')
+        rospy.wait_for_service('algae_to_coord') ## node service name
+        try:
+            img_service = rospy.ServiceProxy('algae_to_coord', camera_uav) ## variable next to the service
+            resp = img_service(img, coord)
+            output_coord = []
+            for i in range(len(resp.x)):
+                output_coord.append((resp.x[i], resp.y[i]))
+                #print(resp.y[i])
+
+            flock_coord = output_coord
+            print(flock_coord)
+
+        except rospy.ServiceException as e:
+            print("Service call failed: %s"%e)
+            flock_coord = []
 
     def posicao_atual(self):
         return self.x, self.y, self.z
