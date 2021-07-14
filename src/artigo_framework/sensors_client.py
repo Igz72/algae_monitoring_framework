c4944c307c4f69f52fb5b23e7ef382b20fa812f0
Isaac Gaetani Alvarez <gaetanialvarez@gmail.com>
2021-07-13 22:34:09 -0300

    Alteraçao para utilizar o server algae_to_coord somente quando necessario

--- src/artigo_framework/sensors_client.py
+++ src/artigo_framework/sensors_client.py
@@ -11,25 +11,20 @@ class Sensors:
         self.x              = 0.0
         self.y              = 0.0
         self.z              = 0.0
+        self.imagem         = Image()
+        self.coordenada     = Odometry()
         self.flock_coord_x  = []
         self.flock_coord_y  = []
 
         if opcao == "global_position":
-            rospy.Subscriber(
-                "/uav1/mavros/global_position/local",
-                Odometry                            ,
-                self.global_position_callback       )
-
+            rospy.Subscriber("/uav1/mavros/global_position/local", Odometry, self.global_position_callback)
         elif opcao == "ground_truth":
-            rospy.Subscriber(
-                "/uav1/ground_truth_pose"   ,
-                PoseWithCovarianceStamped   ,
-                self.ground_truth_callback  )
-        
+            rospy.Subscriber("/uav1/ground_truth_pose", PoseWithCovarianceStamped, self.ground_truth_callback)
+
         image_sub   = message_filters.Subscriber('/uav1/bluefox_optflow/image_raw', Image)
         GPS_sub     = message_filters.Subscriber('/uav1/mavros/global_position/local', Odometry)
         ts          = message_filters.ApproximateTimeSynchronizer([image_sub, GPS_sub], 10, 0.05)
-        ts.registerCallback(self.callbackImg_coord)
+        ts.registerCallback(self.img_coord_callback)
 
     def global_position_callback(self, data):
         self.x = data.pose.pose.position.x
@@ -41,28 +36,21 @@ class Sensors:
         self.y = data.pose.pose.position.y
         self.z = data.pose.pose.position.z
     
-    def callbackImg_coord(self, img, coord):
-        # print
-        rospy.wait_for_service('algae_to_coord') ## node service name
-        try:
-            img_service = rospy.ServiceProxy('algae_to_coord', CameraUAV) ## variable next to the service
-            resp = img_service(img, coord)
-            self.flock_coord_x = resp.x
-            self.flock_coord_y = resp.y
-            # output_coord = []
-            # for i in range(len(resp.x)):
-            #     output_coord.append((resp.x[i], resp.y[i]))
-            #     #print(resp.y[i])
-
-            # flock_coord = output_coord
-            # # print(flock_coord)
-
-        except rospy.ServiceException as e:
-            rospy.loginfo("O serviço falhou em identificar algas")
-            flock_coord = []
+    def img_coord_callback(self, img, coord):
+        self.imagem = img
+        self.coordenada = coord
 
     def posicao_atual(self):
         return self.x, self.y, self.z
     
     def path_planning_algas(self):
-        return self.flock_coord_x, self.flock_coord_y
+        rospy.wait_for_service('algae_to_coord')
+        try:
+            img_service = rospy.ServiceProxy('algae_to_coord', CameraUAV)
+            resp = img_service(self.imagem, self.coordenada)
+
+        except rospy.ServiceException as e:
+            rospy.loginfo("O serviço falhou em identificar algas")
+            return [], []
+
+        return resp.x, resp.y
